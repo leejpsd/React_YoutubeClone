e7f1e8baab871fb0a25f7951638244dd77ded16e
leejpsd <wndvy7690@nate.com>
2023-02-07 02:47:35 +0900

    first

---
+++ src/components/SearchHeader.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { BsSearch, BsYoutube } from "react-icons/bs";
+import { Link, useNavigate, useParams } from "react-router-dom";
+
+export default function SearchHeader() {
+  const navigate = useNavigate();
+  const { keyword } = useParams();
+  const [text, setText] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+
+  useEffect(() => {
+    setText(keyword || "");
+  }, [keyword]);
+  return (
+    <header>
+      <Link to="/">
+        <BsYoutube />
+        <h1>Youtube</h1>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
+}
