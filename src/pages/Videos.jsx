38ec708955065e98b2610b0402cd7e515f1c87e6
leejpsd <wndvy7690@nate.com>
2023-02-08 03:02:51 +0900

    reactQuery

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -1,8 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
 import React from "react";
 import { useParams } from "react-router-dom";
+import VideoCard from "../components/VideoCard";
 
 export default function Videos() {
   const { keyword } = useParams();
+  const {
+    isLoading,
+    error,
+    data: videos,
+  } = useQuery(["videos", keyword], async () => {
+    return fetch(`/videos/${keyword ? "search" : "popular"}.json`)
+      .then((res) => res.json())
+      .then((data) => data.items);
+  });
 
-  return <div>Videos {keyword ? `🔍${keyword}` : "❤️‍🔥"}</div>;
+  return (
+    <>
+      <div>Videos {keyword ? `🔍${keyword}` : "❤️‍🔥"}</div>
+      {isLoading && <p>loading...</p>}
+      {error && <p>error...</p>}
+      {videos && (
+        <ul>
+          {videos.map((video) => (
+            <VideoCard key={video.id} video={video} />
+          ))}
+        </ul>
+      )}
+    </>
+  );
 }
